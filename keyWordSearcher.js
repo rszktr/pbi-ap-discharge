7ac6e6c2d4b7c5bffcde037d00df614ce068ac9f
rszktr <reszketo.robert@gmail.com>
2025-02-15 21:08:39 +0100

    first commit

---
+++ keyWordSearcher.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+const pdf = require('pdf-parse');
+const path = require('path'); // Import the path module for better path handling
+
+let dataBuffer = fs.readFileSync(`./discharge_papers/7c799b38-534f-4abb-9607-78206e23697d.pdf`);
+
+pdf(dataBuffer).then(function (data) {
+    const outputFilePath = path.join(__dirname, 'output.txt'); // Define output path
+
+    let searchResults = []
+
+    //Clean text
+    let regexFooter = /e-MedSolution[\s\S]*?(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2})/gm;
+    let cleanText = data.text.replace(regexFooter, "");
+
+    // RegExps
+    const regexTubeFeeding = / NG| NJ|szond|Szond/gm;
+    const regexSolidFeeding = /melléeh|hozzátá|per os| ehet|eszik|etetés|szilárd/gmi;
+    const regexCholangitis = /cholang|cholecysti|gyulladás/gmi;
+    const regexAntibiotics = /antibio| AB/gmi;
+    const regexPeripancreaticFluid = /peripa|\bfolyadék(?:[^pótl][^fogy]\w*)\b/gmi;
+    const regexPseudocyst = /pseudo|pszeudo|cyst|ciszt/gmi;
+    const regexNecrosis = /necr|nekr/gmi;
+    const regexWon = /won|walled|waled/gmi;
+    const regexRespFailure = /léleg|légz|o2|oxigén|oxigen|elégtelen/gmi;
+    const regexCirculatoryFailure = /kering|adren|elégtelen/gmi;
+    const regexAspiration = /aspir|félrenyel|félre nyel/gmi;
+    const regexDelirium = /delir|delí|pszichoti/gmi;
+    const regexLC = / lc|cholecyste|epehólyag elt|epehólyag-elt|epehólyageltá/gmi;
+
+    const regexArray = [
+        { name: "Szondatáplálás", regex: regexTubeFeeding },
+        { name: "Melléetetés", regex: regexSolidFeeding },
+        { name: "Cholangitis, cholecystitis", regex: regexCholangitis },
+        { name: "Antibiotikum", regex: regexAntibiotics },
+        { name: "Peripankreatikus folyadék", regex: regexPeripancreaticFluid },
+        { name: "Pszeudociszta", regex: regexPseudocyst },
+        { name: "Nekrózis", regex: regexNecrosis },
+        { name: "WON", regex: regexWon },
+        { name: "Légzési elégtelenség", regex: regexRespFailure },
+        { name: "Keringési elégtelenség", regex: regexCirculatoryFailure },
+        { name: "Aspiráció", regex: regexAspiration },
+        { name: "Delírium", regex: regexDelirium },
+        { name: "Cholecystectomia, LC", regex: regexLC },
+    ]
+
+    // Functiom
+    function findMatchInContext(text, regex) {
+        const matches = [];
+        let match;
+
+        while ((match = regex.exec(text)) !== null) {
+            const matchStart = match.index;
+            const matchEnd = matchStart + match[0].length;
+
+            const contextStart = Math.max(0, matchStart - 50);
+            const contextEnd = Math.min(text.length, matchEnd + 50)
+
+            let context = text.substring(contextStart, contextEnd);
+
+            context = context.replace(/\r?\n/g, " "); // Replace newlines with spaces
+
+            // Add "..."
+            if (contextStart > 0) {
+                context = "..." + context;
+            }
+            if (contextEnd < text.length) {
+                context = context + "...";
+            }
+
+            matches.push(context);
+        }
+
+        return matches;
+    }
+
+    // Operation
+    regexArray.forEach(item => {
+        const contexts = findMatchInContext(cleanText, item.regex);
+        
+        resultObject = {
+            name: item.name,
+            regex: item.regex.source,
+            results: contexts
+        }
+
+        searchResults.push(resultObject)
+
+        console.log(`${item.name}:`)
+        contexts.length > 0 ? console.log(contexts) : console.log("No match")
+    })
+
+    console.log(searchResults)
+
+// 
+    const textToWrite = `
+    Number of pages: ${data.numpages}
+${ JSON.stringify(searchResults, null, 2)}
+        `;
+
+    fs.writeFile(outputFilePath, textToWrite, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+        } else {
+            console.log("Data written to output.txt successfully!");
+        }
+    });
+});
---
+++ pdfparse.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const pdf = require('pdf-parse');
+const path = require('path'); // Import the path module for better path handling
+
+let dataBuffer = fs.readFileSync(`./discharge_papers/7c799b38-534f-4abb-9607-78206e23697d.pdf`);
+
+pdf(dataBuffer).then(function (data) {
+
+    // ... (your existing console.log statements)
+
+    const outputFilePath = path.join(__dirname, 'output.txt'); // Define output path
+
+    // Create a string with the data you want to write
+    const textToWrite = `
+Number of pages: ${data.numpages}
+Number of rendered pages: ${data.numrender}
+PDF Info: ${JSON.stringify(data.info, null, 2)}  // Pretty-print JSON
+PDF Metadata: ${JSON.stringify(data.metadata, null, 2)} // Pretty-print JSON
+PDF.js Version: ${data.version}
+PDF Text: ${data.text}
+`;
+
+    let singleRegexSearch = (regexValue) => {
+        const searchResult = data.text.match(regexValue)
+        return searchResult ? searchResult[1] : 'No match found.'
+    }
+
+    let getSpecificSegment = (regexStart, regexEnd) => {
+
+        const multilineRegex = new RegExp(`(?<=(${regexStart.source}))(.*?)(?=(${regexEnd.source}))`, 'sm'); // 's' and 'm' flags
+        const multilineMatch = data.text.match(multilineRegex);
+
+        if (multilineMatch) {
+            const capturedMultilineText = multilineMatch[2];
+            return capturedMultilineText.trim(); // Trim whitespace
+        } else {
+            return "No match found.";
+        }
+
+    }
+
+    const regexPatientName = /Beteg neve\.\.\.:(.*?)(?=KBA\.)/; // Non-greedy matching
+    const regexSegmentLabResults = { start: /Vizsgálatok/, end: /Epikrízis/ }
+
+
+    let patientName = singleRegexSearch(regexPatientName)
+
+    // Find CRP values
+    let segmentLab = getSpecificSegment(regexSegmentLabResults.start, regexSegmentLabResults.end)
+
+    function removePrintedByDates(text) {
+        const regex = /Nyomtatva:.*?(\d{4}\.\d{2}\.\d{2})/gs; // Regex to find dates after "Nyomtatva:"
+        return text.replace(regex, ""); // Replace matches with empty string
+      }
+
+    segmentLab = removePrintedByDates(segmentLab)
+
+    const matches2 = Array.from(segmentLab.matchAll(/CRP\s*([\d.]+)\s*mg\/L/g), match => parseFloat(match[1]));
+
+    // Create Date segments
+    const crpValues2 = {};
+    for (const match of segmentLab.matchAll(/(\d{4}\.\d{2}\.\d{2})\s+[\s\S]*?CRP\s*([\d.]+)\s*mg\/L/g)) {
+        const date = match[1];
+        const crpValue = parseFloat(match[2]); // Parse to float
+    
+        crpValues2[date] = crpValues2[date] || []; // Initialize array if date doesn't exist
+        crpValues2[date].push(crpValue);
+    }
+    
+    console.log(crpValues2);
+
+    console.log(`Max. CRP: ${Math.max(...Object.values(crpValues2).flat())}`)
+
+
+
+
+    fs.writeFile(outputFilePath, textToWrite, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+        } else {
+            console.log("Data written to output.txt successfully!");
+        }
+    });
+
+    fs.appendFile(outputFilePath, segmentLab, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+        } else {
+            console.log("Data written to output.txt successfully!");
+        }
+    })
+});
